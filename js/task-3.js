9f9f0b93fc6079df92f87eea88e106c26ffac308
Olia <olyasemenchuk13@gmail.com>
2025-12-07 16:22:28 +0200

    corrected one task

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 function filterArray(numbers, value) {
     const arr = []
-    for (let i = 0; i <= numbers.length; i++) {
+    for (let i = 0; i < numbers.length; i++) {
         if (numbers[i] > value) {
             arr.push(numbers[i])
         }
